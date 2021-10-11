36b7c7f5993d30ac767e12f05591a2b35746eeb7
randynetworks <randyramadhan1973@gmail.com>
2021-10-11 12:41:24 +0700

    Feat: add project until UAS

---
+++ UAS/app/src/main/java/krisantus/android/uas/FormInput.java
@@ -0,0 +1,14 @@
+package krisantus.android.uas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormInput extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_input);
+    }
+}
